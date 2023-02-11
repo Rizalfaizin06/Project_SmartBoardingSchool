8a8c39d41ccb1836f2497854928545bdd6b3f3fe
Rizalfaizin06 <rizalfaizin06@gmail.com>
2023-02-12 00:33:33 +0700

    membuat fitur order dengan list barang dan harga total

--- ujicoba2/index.php
+++ ujicoba2/index.php
@@ -3,42 +3,53 @@ require 'fungsiAdmin.php';
 $menu = query("SELECT * FROM tbl_menu");
 
 if (isset($_POST['buttonPesan'])) {
-  $idMenu = $_POST['idMenu'];
-  // $jumlahPesan = $_POST['jumlahPesan'];
-  // query("INSERT INTO tbl_pesan (idPesan, idPembeli, idOrder, idMenu, jumlahPesan) VALUES (NULL, 1, 0, $idMenu, 0);");
-  $query = "INSERT INTO tbl_pesan (idPesan, idPembeli, idOrder, idMenu, jumlahPesan) VALUES (NULL, 1, 0, $idMenu, 0);";
+    $idMenu = $_POST['idMenu'];
+    // $jumlahPesan = $_POST['jumlahPesan'];
+    // query("INSERT INTO tbl_pesan (idPesan, idPembeli, idOrder, idMenu, jumlahPesan) VALUES (NULL, 1, 0, $idMenu, 0);");
+    $query = "INSERT INTO tbl_pesan (idPesan, idPembeli, idOrder, idMenu, jumlahPesan) VALUES (NULL, 1, 0, $idMenu, 0);";
 
-  mysqli_query($koneksi, $query);
+    mysqli_query($koneksi, $query);
 
 }
+
+
+
+
 if (isset($_POST['buttonHapus'])) {
-  $idMenu = $_POST['idMenu'];
-  $query = "DELETE FROM tbl_pesan WHERE idMenu = '$idMenu'";
+    $idMenu = $_POST['idMenu'];
+    $query = "DELETE FROM tbl_pesan WHERE idMenu = '$idMenu'";
 
-  mysqli_query($koneksi, $query);
+    mysqli_query($koneksi, $query);
 
 }
+
+
+
+
 if (isset($_POST['buttonOrder'])) {
-  // $idPembeli = $_POST['idPembeli'];
-  $waktuOrder = date("Y-m-d H:i:s");
+    // $idPembeli = $_POST['idPembeli'];
+    $waktuOrder = date("Y-m-d H:i:s");
 
-  $query = "INSERT INTO `tbl_order` (`idOrder`, `waktuOrder`, `statusOrder`) VALUES (NULL, '$waktuOrder', 'belum')";
-  mysqli_query($koneksi, $query);
+    $query = "INSERT INTO tbl_order (idOrder, waktuOrder, statusOrder) VALUES (NULL, '$waktuOrder', 'belum')";
+    mysqli_query($koneksi, $query);
 
-  $queryidOrder = query("SELECT idOrder FROM tbl_order ORDER BY idOrder DESC LIMIT 1");
-  $idOrder = $queryidOrder[0]["idOrder"];
-  var_dump($idOrder);
+    $querydOrder = query("SELECT idOrder FROM tbl_order ORDER BY idOrder DESC LIMIT 1");
+    $idOrder = $querydOrder[0]["idOrder"];
+    var_dump($idOrder);
 
-  $query = "INSERT INTO `tbl_order` (`idOrder`, `waktuOrder`, `statusOrder`) VALUES (NULL, '$waktuOrder', 'belum')";
-  mysqli_query($koneksi, $query);
 
-  $dataPesanan = query("SELECT * FROM tbl_pesan WHERE idOrder = 0");
-  foreach($dataPesanan as $oneView) {
+
+    $dataPesanan = query("SELECT * FROM tbl_pesan WHERE idOrder = 0");
+
+    foreach($dataPesanan as $oneView) {
     $idMenu = $oneView['idMenu'];
     $jumlahPesan = $_POST['jumlahPesan'.$idMenu];
     $queryUbahJumlah = "UPDATE tbl_pesan SET jumlahPesan = $jumlahPesan, idOrder = $idOrder WHERE idOrder = 0 AND idMenu = $idMenu";
     mysqli_query($koneksi, $queryUbahJumlah);
-  }
+    }
+    // SELECT hargaMenu, jumlahPesan, hargaMenu * jumlahPesan AS total FROM tbl_pesan INNER JOIN tbl_menu ON tbl_pesan.idMenu = tbl_menu.idMenu;
+    header("Location: order.php");
+
 }
 
 
@@ -55,7 +66,7 @@ $pesanan = array();
 //   array_push($pesanan, $readData['idMenu']);
 // }
 foreach($dataPesanan as $oneView) {
-  array_push($pesanan, $oneView['idMenu']);
+    array_push($pesanan, $oneView['idMenu']);
 }
 ?>
 
@@ -63,109 +74,109 @@ foreach($dataPesanan as $oneView) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Kantin</title>
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="assets/icon/bootstrap-icons.css">
-  
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kantin</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/icon/bootstrap-icons.css">
+
 </head>
 <body>
-  <div class="container">
+    <div class="container">
     <?php
-  foreach ($menu as $oneView) :
+    foreach ($menu as $oneView) :
     ?>
     <div class="card shadow p-3 mt-3 mb-3 w-4">
-      <form action="" method="post">
+        <form action="" method="post">
         <h3><?= $oneView["namaMenu"]; ?></h3>
         <h3><?= $oneView["hargaMenu"]; ?></h3>
-				<input type="hidden" name="idMenu" value="<?= $oneView["idMenu"]; ?>">
+                <input type="hidden" name="idMenu" value="<?= $oneView["idMenu"]; ?>">
         <?php
         
         if(in_array($oneView["idMenu"], $pesanan)):?>
-          <button class="btn btn-secondary w-25" type="submit" disabled>Sudah dipesan</button>
-          <?php else:?>
+            <button class="btn btn-secondary w-25" type="submit" disabled>Sudah dipesan</button>
+            <?php else:?>
             <button class="btn btn-outline-dark w-25" type="submit" id="button-pesan" name="buttonPesan">Pesan</button>
 
             <?php endif;?>
-			</form>
-      
+            </form>
+        
     </div>
     <?php endforeach; ?>
     <form action="" method="post">
-      <div class="card shadow p-3 mt-3 mb-3 w-4">
+        <div class="card shadow p-3 mt-3 mb-3 w-4">
         <table class="table" id="tableLogData">
-          <thead class="table-light">
+            <thead class="table-light">
             <tr>
-              <!-- <th>No.</th> -->
-              <th>Menu</th>
-              <th>Jumlah</th>
-              <th>Action</th>
+                <!-- <th>No.</th> -->
+                <th>Menu</th>
+                <th>Jumlah</th>
+                <th>Action</th>
             </tr>
-          </thead>
-          <tbody class="table-group-divider">
+            </thead>
+            <tbody class="table-group-divider">
             <?php
-              foreach ($dataPesanan as $oneView):
-                  ?>
-                  <tr>
-                  <td><?=$oneView["idMenu"];?>
-                  <input id="<?= 'harga'.$oneView['idMenu']; ?>" class="span8" type="hidden" value="50000"/>
-                  </td>
-                  <td><input id="<?= 'jumlahPesan'.$oneView['idMenu']; ?>" type="number" name="<?= 'jumlahPesan'.$oneView['idMenu']; ?>" value="1" onchange="return operasi()">
-                  </td> 
-                  <td>
-                  <form action="" method="post">
-                    <input type="hidden" name="idMenu" value="<?= $oneView["idMenu"]; ?>">
-                    <button class="btn btn-outline-dark w-3" type="submit" id="buttonHapus" name="buttonHapus">Hapus</button>
-                  </form>
-                  </td>
-                  </tr>
-              <?php endforeach; 
-              if ((empty($dataPesanan))) {
-                  echo "<tr><td class='text-center' colspan='5' style='color: red; font-style: italic; font-size: 20px;'>Belum Ada Pesanan</td></tr>";
-              }
-              ?>
-          </tbody>
+                foreach ($dataPesanan as $oneView):
+                    ?>
+                    <tr>
+                    <td><?=$oneView["idMenu"];?>
+                    <input id="<?= 'harga'.$oneView['idMenu']; ?>" class="span8" type="hidden" value="50000"/>
+                    </td>
+                    <td><input id="<?= 'jumlahPesan'.$oneView['idMenu']; ?>" type="number" name="<?= 'jumlahPesan'.$oneView['idMenu']; ?>" value="1" onchange="return operasi()">
+                    </td> 
+                    <td>
+                    <form action="" method="post">
+                        <input type="hidden" name="idMenu" value="<?= $oneView["idMenu"]; ?>">
+                        <button class="btn btn-outline-dark w-3" type="submit" id="buttonHapus" name="buttonHapus">Hapus</button>
+                    </form>
+                    </td>
+                    </tr>
+                <?php endforeach; 
+                if ((empty($dataPesanan))) {
+                    echo "<tr><td class='text-center' colspan='5' style='color: red; font-style: italic; font-size: 20px;'>Belum Ada Pesanan</td></tr>";
+                }
+                ?>
+            </tbody>
         </table>
         <div class="container">
-          <h3>Total Harga : Rp. <span id="spanTotalHarga">0</span></h3>
-          
-          <input class="span8" id="tot" name="total_harga" type="hidden" value="" placeholder="" />
+            <h3>Total Harga : Rp. <span id="spanTotalHarga">0</span></h3>
+            
+            <input class="span8" id="tot" name="total_harga" type="hidden" value="" placeholder="" />
         </div>
         <input type="hidden" name="idMenu" value="<?= $oneView["idMenu"]; ?>">
         <button class="btn btn-outline-dark w-25" type="submit" id="buttonOrder" name="buttonOrder">Order</button>
-      </div>
+        </div>
     </form>
-  </div>
+    </div>
 
 
-  
-  
-  <script src="assets/js/jquery-3.6.0.min.js"></script>
-  <script type="text/javascript">
+
+
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript">
     function operasi(){
-      var pesan = new Array();
-      var jumlah = new Array();
-      var total = 0;
-      for(var a = 0; a < 1000; a++){
+        var pesan = new Array();
+        var jumlah = new Array();
+        var total = 0;
+        for(var a = 0; a < 1000; a++){
         pesan[a] = $("#harga"+a).val();
         jumlah[a] = $("#jumlahPesan"+a).val();
-      } 
-      for(var a = 0; a < 1000; a++){
+        } 
+        for(var a = 0; a < 1000; a++){
         if(pesan[a] == null || pesan[a] == ""){
-          pesan[a] = 0;
-          jumlah[a] = 0;
+            pesan[a] = 0;
+            jumlah[a] = 0;
         }
         total += Number(pesan[a] * jumlah[a]);
-      }
-      
-      //alert(total);
-      $("#spanTotalHarga").text(total);
-      $("#tot").val(total);
+        }
+        
+        //alert(total);
+        $("#spanTotalHarga").text(total);
+        $("#tot").val(total);
     }
-    
+
     operasi();
-  </script>
+    </script>
 </body>
 </html>
